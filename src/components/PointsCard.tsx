6a0fdc91242a08c11127f0562a3e62b1a7225ce6
Patrik Cmil <cmil.patrik@gmail.com>
2025-09-12 09:33:48 +0200

    bb logo update

--- src/components/PointsCard.tsx
+++ src/components/PointsCard.tsx
@@ -5,7 +5,7 @@ const teamLogos: Record<string, string> = {
   'HK SNV': 'https://www.hokejportal.net/hp/kluby/SNV.png?202508b',
   'HK NR': 'https://www.hokejportal.net/hp/kluby/NIT.png?202508b',
   'HC ZA': 'https://www.hokejportal.net/hp/kluby/ZIL.png?202508b',
-  'HC BB': 'https://www.hokejportal.net/hp/kluby/PRE.png?202508b',
+  'HC BB': 'https://www.hokejportal.net/hp/kluby/BBY.png?202508b',
   'HK PP': 'https://www.hokejportal.net/hp/kluby/POP.png?202508b',
   'HC BA': 'https://www.hokejportal.net/hp/kluby/SBA.png?202508b',
   'HK TN': 'https://www.hokejportal.net/hp/kluby/TRE.png?202508b',
